816cf653683d4ee8464f54ff631ecd59576f5d99
SHOAIB-RAFIQ <55207086+SHOAIB-RAFIQ@users.noreply.github.com>
2019-10-21 15:28:21 +0500

    Shoaib4

--- cStack.cpp
+++ cStack.cpp
@@ -99,3 +99,54 @@ public:
 
 };
 #pragma once
+Include Irvine32.inc
+.data
+arr0 byte "if you have bsc enter 1 otherwise 0",0
+arr byte "Enter your age:",0
+arr1 byte "The candidate is eligible",0
+arr2 byte "The candidate isn't  eligible",0
+age dword ?
+qual dword ?
+
+.code
+main proc
+
+mov edx,offset arr
+call writestring
+call crlf
+mov eax,0
+call readint
+cmp eax,22
+
+je L3
+jg L3
+jl L3
+
+L1:
+ 
+    mov edx,offset arr1
+	call writestring
+	jmp skip
+L2:
+
+    mov edx,offset arr1
+	call writestring
+	jmp skip
+L3:
+    mov edx,offset arr0
+	call writestring
+	call readint
+	mov qual,eax
+	cmp eax,1
+	je L1
+	jl skip
+	jg skip
+
+skip:
+	mov edx,offset arr2
+	call writeint 
+
+call readint
+exit
+main endp
+end main
